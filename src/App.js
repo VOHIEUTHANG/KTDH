8de2cad0004d925e3232b21d539ccd13e2d3c2de
VOHIEUTHANG <hieuthang369@gmail.com>
2022-06-04 14:07:14 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        This is my first project that I'm using material UI and Tailwind Css
+      </h1>
+      <Button className="" variant="outlined">
+        Click me
+      </Button>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  importants: "#root",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
