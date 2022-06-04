34e9ac74a326cd6bbe504bf6eab7f54e0366a18b
VOHIEUTHANG <hieuthang369@gmail.com>
2022-06-04 17:37:22 +0700

    add new coodinate component

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -82,6 +82,38 @@ function Controls() {
             />
           </div>
         </div>
+        <hr className="my-4" />
+        <div className="flex items-center">
+          <div className="w-1/2 flex justify-start  items-center">
+            <Avatar src="" sx={{ width: 100, height: 100 }} />
+          </div>
+          <div className="flex flex-col ">
+            <TextField
+              className="my-3"
+              id="outlined-read-only-input"
+              label="X value"
+              defaultValue="10"
+              InputProps={{
+                readOnly: true,
+                endAdornment: (
+                  <InputAdornment position="end">pixel</InputAdornment>
+                ),
+              }}
+            />
+            <TextField
+              className="my-3"
+              id="outlined-read-only-input"
+              label="X value"
+              defaultValue="50"
+              InputProps={{
+                readOnly: true,
+                endAdornment: (
+                  <InputAdornment position="end">pixel</InputAdornment>
+                ),
+              }}
+            />
+          </div>
+        </div>
       </div>
 
       <Button
